4daa386efcfb926947b05783600bbfb43b383870
hana-ri <hm.rizal1933@gmail.com>
2023-12-27 16:52:16 +0700

    update (fixed some bugs): final fixed some bugs (Rabu, 27122023)

--- app/src/main/java/com/example/projecttugasbesarardi/AddPasien.java
+++ app/src/main/java/com/example/projecttugasbesarardi/AddPasien.java
@@ -2,19 +2,26 @@ package com.example.projecttugasbesarardi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AddPasien extends AppCompatActivity {
+public class AddPasien extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     protected Cursor cursor;
     DataHelper dbHelper;
     EditText text1, text2, text3, text4, text5;
+
+    String[] gender = { "Laki-Laki", "Perempuan"};
+    int spinner_position = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +30,9 @@ public class AddPasien extends AppCompatActivity {
         dbHelper = new DataHelper(this);
         text1 = (EditText) findViewById(R.id.idnik);
         text2 = (EditText) findViewById(R.id.idnama);
-        text3 = (EditText) findViewById(R.id.idjk);
+//        text3 = (EditText) findViewById(R.id.idjk);
+        Spinner spin = (Spinner) findViewById(R.id.idjk);
+        spin.setOnItemSelectedListener(this);
         text4 = (EditText) findViewById(R.id.idttl);
         text5 = (EditText) findViewById(R.id.idalamat);
         String diagnosa = "Belum ada";
@@ -31,6 +40,11 @@ public class AddPasien extends AppCompatActivity {
 
         Button btn3 = findViewById(R.id.buttonBack);
         Button btn4 = findViewById(R.id.buttonAddPasien);
+
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,gender);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
 
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,19 +58,30 @@ public class AddPasien extends AppCompatActivity {
             public void onClick(View arg0) {
                 // TODO Auto-generated method stub
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
-                db.execSQL("insert into biodata(nik, nama, tgl_lahir, jk, alamat, diagnosa, verifikasi) values('" +
+                String query = "insert into biodata(nik, nama, tgl_lahir, jk, alamat, diagnosa, verifikasi) values('" +
                         text1.getText().toString() + "','" +
                         text2.getText().toString() + "','" +
-                        text3.getText().toString() + "','" +
                         text4.getText().toString() + "','" +
+                        gender[spinner_position].toString() + "','" +
                         text5.getText().toString() + "','" +
                         diagnosa + "','" +
-                        verifikasi + "')" );
+                        verifikasi + "')";
+                db.execSQL(query);
                 Toast.makeText(getApplicationContext(), "Berhasil Tambah Pasien", Toast.LENGTH_LONG).show();
                 Dashboard.ma.RefreshList();
                 finish();
             }
         });
 
+    }
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        spinner_position = position;
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
--- app/src/main/java/com/example/projecttugasbesarardi/UpdatePasien.java
+++ app/src/main/java/com/example/projecttugasbesarardi/UpdatePasien.java
@@ -3,20 +3,30 @@ package com.example.projecttugasbesarardi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UpdatePasien extends AppCompatActivity {
+import java.util.Arrays;
+
+public class UpdatePasien extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     protected Cursor cursor;
     DataHelper dbHelper;
     Button ton1, ton2;
     EditText text1, text2, text3, text4, text5;
+
+    String[] gender = { "Laki-Laki", "Perempuan"};
+
+    int spinner_position = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +35,9 @@ public class UpdatePasien extends AppCompatActivity {
         dbHelper = new DataHelper(this);
         text1 = (EditText) findViewById(R.id.idnikUpdate);
         text2 = (EditText) findViewById(R.id.idnamaUpdate);
-        text3 = (EditText) findViewById(R.id.idjkUpdate);
+//        text3 = (EditText) findViewById(R.id.idjkUpdate);
+        Spinner spin = (Spinner) findViewById(R.id.idjkUpdate);
+        spin.setOnItemSelectedListener(this);
         text4 = (EditText) findViewById(R.id.idttlUpdate);
         text5 = (EditText) findViewById(R.id.idalamatUpdate);
 
@@ -35,15 +47,24 @@ public class UpdatePasien extends AppCompatActivity {
                 Integer.parseInt(getIntent().getStringExtra("no_id")) + "'",null);
         cursor.moveToFirst();
 
+        String myString = "Laki-Laki";
+
         if (cursor.getCount()>0)
         {
             cursor.moveToPosition(0);
             text1.setText(cursor.getString(1).toString());
             text2.setText(cursor.getString(2).toString());
-            text3.setText(cursor.getString(3).toString());
-            text4.setText(cursor.getString(4).toString());
+//            text3.setText(cursor.getString(3).toString());
+            myString = cursor.getString(4).toString();
+            text4.setText(cursor.getString(3).toString());
             text5.setText(cursor.getString(5).toString());
         }
+
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,gender);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+        spin.setSelection(Arrays.asList(gender).indexOf(myString));
 
         Button btnBackUpdate = findViewById(R.id.buttonBackUpdate);
         btnBackUpdate.setOnClickListener(new View.OnClickListener() {
@@ -63,7 +84,7 @@ public class UpdatePasien extends AppCompatActivity {
                 db.execSQL("update biodata set " +
                         "nik='"+ text1.getText().toString() +"', " +
                         "nama='"+ text2.getText().toString() +"', " +
-                        "jk='" + text3.getText().toString()+"', " +
+                        "jk='" + gender[spinner_position].toString() +"', " +
                         "tgl_lahir='" + text4.getText().toString() +"', " +
                         "alamat='" + text5.getText().toString() + "' " +
                         "where no='" + Integer.parseInt(getIntent().getStringExtra("no_id")) +"'");
@@ -72,5 +93,15 @@ public class UpdatePasien extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        spinner_position = position;
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
